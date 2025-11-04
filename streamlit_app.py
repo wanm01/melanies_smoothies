f63303e3ecb379c2c444312c35d0b21f4b312c57
Mandy <87651480+wanm01@users.noreply.github.com>
2025-11-04 15:08:09 +0000

    Update streamlit_app.py
    change indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,13 @@ if ingredients_list:
     # st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+
 # new section to display nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
 
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
