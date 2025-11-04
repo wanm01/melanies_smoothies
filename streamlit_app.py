7904e2fbfc79f25c9d7869aa03c2b296d3f1d677
Mandy <87651480+wanm01@users.noreply.github.com>
2025-11-04 15:15:25 +0000

    Update streamlit_app.py
    using chosen fruit variable in api call for titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,8 @@ if ingredients_list:
 # for each fruit_chosen in ingredients_list multiselect box: do everything below this line that is indented. 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
